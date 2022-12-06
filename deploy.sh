40df6a1766947c65d9e13f9170190c924e5f4d9c
XrayBravoGolf <xuxiaoyao213@gmail.com>
2022-12-06 15:21:22 -0500

    app configs

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+sam package --s3-bucket ascending-devops --output-template-file packaged.yaml
+sam deploy --template-file packaged.yaml --region us-east-1 --capabilities CAPABILITY_IAM --stack-name $1-api --parameter-overrides ParameterKey=Environment,ParameterValue=$1 ParameterKey=Version,ParameterValue=$2
