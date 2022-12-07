4466423915ac1cff9641d602c299ea1b7d0d504b
XrayBravoGolf <xuxiaoyao213@gmail.com>
2022-12-07 10:54:08 -0500

    update pipeline command

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -42,4 +42,4 @@ class TestApiGateway:
         response = requests.get(api_gateway_url)
 
         assert response.status_code == 200
-        assert response.json() == {"message": "hello world 2"}
+        assert response.json() == {"message": "hello world 3"}
